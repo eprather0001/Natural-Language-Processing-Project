ec01abdc7b35962438bcb2d4efa5fa6d7a8ddbc7
eprather0001 <eprather0001@mymail.lausd.net>
2025-03-05 14:23:59 -0800

    Create NLPRunner.java

---
+++ NLPRunner.java
@@ -0,0 +1,22 @@
+public class NLPRunner {
+  public static void main(String[] args) {
+
+    MyNLP loraxScript = new MyNLP ("loraxScript.txt");
+
+    loraxScript.scanner();
+
+  
+
+    
+
+
+
+
+
+
+    
+    
+    
+    
+  }
+}
